0c0cb7907b37c0f9a5c25f472a6ba3cd9ee2adbc
lpc <209539905@qq.com>
2023-10-25 11:24:34 +0800

    add:测试用例:登录页_登录_登录成功

--- src/test/java/com/chances/chancesuser/ChancesUserApplicationTests.java
+++ src/test/java/com/chances/chancesuser/ChancesUserApplicationTests.java
@@ -1,13 +1,39 @@
 package com.chances.chancesuser;
 
+import com.chances.chancesuser.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ChancesUserApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void 登录页_登录_登录成功() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"example\"}")
+                        .param("loginName", "adminlpc")
+                        .param("password", "qwerty"))
+                .andDo(MockMvcResultHandlers.print()) // 打印响应内容
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
