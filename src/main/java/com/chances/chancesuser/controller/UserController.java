fa5e7ea11bed0c3bbb385b76ee343a0db376ffe3
lpc <209539905@qq.com>
2023-10-25 19:19:42 +0800

    fix:请求公共路径提取

--- src/main/java/com/chances/chancesuser/controller/UserController.java
+++ src/main/java/com/chances/chancesuser/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Resource
     private UserService userService;
@@ -20,7 +21,7 @@ public class UserController {
      * @param password  密码
      * @return token
      */
-    @PostMapping(value = "/user/login")
+    @PostMapping(value = "/login")
     private Result userLogin(@RequestParam("loginName") String loginName,
                              @RequestParam("password") String password) {
         String token = userService.userLogin(loginName, password);
@@ -34,7 +35,7 @@ public class UserController {
      * @param userDTO userDTO
      * @return R
      */
-    @PostMapping(value = "/user/add")
+    @PostMapping(value = "/add")
     private Result userAdd(@RequestBody UserDTO userDTO) {
         userService.add(userDTO);
         return Result.ok().setMsg("创建成功");
@@ -47,7 +48,7 @@ public class UserController {
      * @param token token
      * @return ok
      */
-    @GetMapping(value = "/user/logout")
+    @GetMapping(value = "/logout")
     private Result logout(@RequestHeader String token) throws Exception {
         userService.logout(token);
         return Result.ok().setMsg("退出成功");
@@ -61,7 +62,7 @@ public class UserController {
      * @param pageSize pageSize
      * @param pageNum  pageNum
      */
-    @GetMapping(value = "/user/list")
+    @GetMapping(value = "/list")
     private Result userList(@RequestParam(required = false) String email,
                             @RequestParam(required = false) String mobile,
                             @RequestParam(required = false, defaultValue = "10") String pageSize,
@@ -74,7 +75,7 @@ public class UserController {
      *
      * @param userId 用户id
      */
-    @DeleteMapping(value = "/user/{userId}")
+    @DeleteMapping(value = "/{userId}")
     private Result userDelete(@PathVariable String userId) {
         userService.userDelete(userId);
         return Result.ok().setMsg("移除成功");
@@ -86,7 +87,7 @@ public class UserController {
      *
      * @param userId 用户id
      */
-    @GetMapping(value = "/user/{userId}")
+    @GetMapping(value = "/{userId}")
     private Result userInfo(@PathVariable String userId,
                             @RequestHeader String token) {
         return Result.ok(userService.userInfo(userId, token)).setMsg("用户信息");
@@ -97,7 +98,7 @@ public class UserController {
      *
      * @param userId 用户id
      */
-    @PutMapping(value = "/user/{userId}")
+    @PutMapping(value = "/{userId}")
     private Result userUpdate(@PathVariable String userId,
                               @RequestBody UserDTO userDTO,
                               @RequestHeader String token) {
@@ -112,7 +113,7 @@ public class UserController {
      * @param userId 用户id
      * @param status 用户状态
      */
-    @PostMapping(value = "user/update/status")
+    @PostMapping(value = "/update/status")
     private Result lock(@RequestParam("status") String status,
                         @RequestParam("userId") String userId) {
         userService.lock(userId, status);
@@ -126,7 +127,7 @@ public class UserController {
      * @param oldPassword 旧密码
      * @param newPassword 新密码
      */
-    @PostMapping(value = "user/update/password")
+    @PostMapping(value = "/update/password")
     private Result password(@RequestParam("oldPassword") String oldPassword,
                             @RequestParam("newPassword") String newPassword,
                             @RequestHeader String token) throws Exception {
@@ -140,7 +141,7 @@ public class UserController {
      * @param file  文件
      * @param token 用户
      */
-    @PostMapping("/user/avatar/upload")
+    @PostMapping("/avatar/upload")
     @ResponseBody
     public Result uploadFile(@RequestParam("file") MultipartFile file,
                              @RequestHeader String token) {
